f701eb19288d2aff6897e81b7f7e8b838c616f24
Alter <jf.lozano.n@gmail.com>
2025-12-28 23:02:32 -0600

    feat: lint installed

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableHighlight, Pressable } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start</Text>
+      <Text>Some other stuff</Text>
+      <Button title="Pulsa aqui"/>
+      <Pressable underlayColor={"#09f"}
+      onPress={()=>{}} 
+      style={({pressed})=>[
+        {backgroundColor:pressed?"rgb(210,230,255)":"white"},
+        styles.wrapperCustom,
+      ]}>
+        {({pressed})=>(
+          <Text style={{
+            fontSize:pressed?32:16,
+          }}>{pressed?"Presionado":"No presionado"}</Text>
+        )}
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
