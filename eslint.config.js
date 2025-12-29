6ebadfdd3f1fc202a53f1aa841a7909fc2fc95f7
Alter <jf.lozano.n@gmail.com>
2025-12-29 01:51:16 -0600

    feat: metacritic lib added

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,8 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require("eslint-config-expo/flat");
-
-module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ["dist/*"],
+// https://docs.expo.dev/guides/using-esli
+module.exports={
+  extends:['expo','prettier'],
+  plugins:['prettier'],
+  rules:{
+    'prettier/prettier':'error',
   }
-]);
+};
